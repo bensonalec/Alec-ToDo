aa63b768464efdcf1929980a5dfc5dc14639f467
Alec Benson <alecbenson21@gmail.com>
2020-10-08 17:05:53 -0600

    Yeet

--- frontend/src/containers/List.js
+++ frontend/src/containers/List.js
@@ -1,13 +1,36 @@
-import React from "react";
+// import React from "react";
 import "./List.css";
+import { useAppContext } from "../libs/contextLib";
 
-export default function List() {
-  return (
-    <div className="List">
-      <div className="lander">
-        <h1>Shit happens here</h1>
-        <p>A to do app</p>
-      </div>
-    </div>
-  );
+import React, { Component } from "react";
+
+
+class List extends Component {
+
+  
+  // test() {
+  //   const { userHasAuthenticated } = useAppContext();
+  //   console.log("IN")
+  // }
+  state = {
+    listitems: [["Apple","1"], ["Pear","2"], ["Avocado","3"]],
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <ul className="list-group">
+          {this.state.listitems.map(listitem => (
+            <li className="list-group-item list-group-item-primary">
+              {listitem[0]}
+              <li className="list-group-item list-group-item-secondary">
+                {listitem[1]}
+              </li>
+            </li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
+  }
 }
+export default List;
